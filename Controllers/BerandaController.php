7a75cd7a71e2ae3cd0305f4dfa41a8427ea9c878
chaernh <chaerfansyah.hidayah@gmail.com>
2025-05-28 19:25:19 +0700

    feat: update docs I

--- Controllers/BerandaController.php
+++ Controllers/BerandaController.php
@@ -1,6 +1,19 @@
 <?php
-
+/**
+ * Controller untuk mengelola halaman beranda
+ * 
+ * Controller ini menangani tampilan halaman utama aplikasi perpustakaan.
+ * Berisi method untuk menampilkan halaman beranda dengan layout utama.
+ */
 class BerandaController {
+    /**
+     * Menampilkan halaman beranda
+     * 
+     * Method ini akan:
+     * 1. Mengatur judul halaman
+     * 2. Mengambil konten dari view beranda
+     * 3. Menampilkan konten dalam layout utama
+     */
     public function index() {
         $title = 'Beranda - Perpustakaan';
         ob_start();
